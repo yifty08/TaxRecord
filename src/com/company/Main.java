9b8b178daa4c0eb8198049fcd1352e4f219cfea0
Yifty <yifty08@icloud.com>
2019-04-16 15:07:55 -0400

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,31 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("number: ");
-        Integer number = scanner.nextInt();
-        String numStr = number.toString();
+        System.out.println("name: ");
+        String name = scanner.nextLine();
 
-        System.out.println("This number is a string: " + numStr);
+        System.out.println("Purchase Amount: ");
+        int purchaseAmount = scanner.nextInt();
+
+        System.out.println("Tax code(pick from 0-3): ");
+        int taxCode = scanner.nextInt();
+
+        int salesTax = -1;
+
+        if (taxCode==0){
+            salesTax=0;
+        } else if (taxCode==1){
+            salesTax=3;
+        } else if(taxCode==2){
+            salesTax=5;
+        } else if (taxCode==3){
+            salesTax=7;
+        }
+
+        double totalAmountDue = purchaseAmount + purchaseAmount * salesTax/100;
+
+        System.out.println("Name is: " + name + ", purchase Amount is " +
+                purchaseAmount + ", sales tax is " + salesTax +
+        "%, and total amount due is: " + totalAmountDue + " .");
     }
 }
